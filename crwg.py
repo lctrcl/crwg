7f0f6aacbb21cfcf74569d68ec75a30eb09e2c1f
n0kovo <n0kovo@riseup.net>
2022-09-07 16:13:10 +0200

    Misc style changes

--- crwg.py
+++ crwg.py
@@ -5,7 +5,9 @@ import time
 import os
 import sys
 import re
-import urllib.request, urllib.parse, urllib.error
+import urllib.request
+import urllib.parse
+import urllib.error
 import zipfile
 import bz2
 from tqdm import tqdm
@@ -16,20 +18,22 @@ from transliterate import translit, get_available_language_codes
 from transliterate.discover import autodiscover
 
 from collections import Counter
+
 # init
 __author__ = "Igor Ivanov, @lctrcl"
 __license__ = "GPL"
 __version__ = "0.3"
 __banner__ = f"Custom Russian Wordlists Generator {__version__}"
 
-dictionary_urls = {'ruscorpora': 'https://ruscorpora.ru/new/ngrams/1grams-3.zip',
-                   'opencorpora': 'http://opencorpora.org/files/export/ngrams/unigrams.cyr.lc.bz2'}
+dictionary_urls = {
+    "ruscorpora": "https://ruscorpora.ru/new/ngrams/1grams-3.zip",
+    "opencorpora": "http://opencorpora.org/files/export/ngrams/unigrams.cyr.lc.bz2",
+}
 
 
 class MyParser(argparse.ArgumentParser):
-
     def error(self, message):
-        sys.stderr.write(f'error: {message}\n')
+        sys.stderr.write(f"error: {message}\n")
         self.print_help()
         sys.exit(2)
 
@@ -43,17 +47,17 @@ class ReverseInverseRussianLanguagePack(TranslitLanguagePack):
     mapping = (
         "йцукенгшщзхъфывапролджэёячсмитьбю",
         "qwertyuiop[]asdfghjkl;'\\zxcvbnm,.",
-    )   
+    )
+
 
 registry.register(ReverseInverseRussianLanguagePack)
-
 
 
 def _reporthook(numblocks, blocksize, filesize, url=None):
     base = os.path.basename(url)
     try:
         percent = min((numblocks * blocksize * 100) / filesize, 100)
-    except:
+    except BaseException:
         percent = 100
     if numblocks != 0:
         sys.stdout.write("\b" * 70)
@@ -64,86 +68,90 @@ def downloaddictionaries(dictionary_strings):
     url = dictionary_urls[dictionary_strings]
 
     try:
-        print(f'[*] Downloading {dictionary_strings} dictionary')
-        name, hdrs = urllib.request.urlretrieve(url, os.path.basename(
-            url), lambda nb, bs, fs, url=url: _reporthook(nb, bs, fs, url))
+        print(f"[*] Downloading {dictionary_strings} dictionary")
+        name, hdrs = urllib.request.urlretrieve(
+            url,
+            os.path.basename(url),
+            lambda nb, bs, fs, url=url: _reporthook(nb, bs, fs, url),
+        )
     except IOError as e:
         print(f"Can't retrieve {url!r}: {e}")
-    if dictionary_strings == 'ruscorpora':
+    if dictionary_strings == "ruscorpora":
         try:
-            print(f'[*] Extracting {dictionary_strings} dictionary')
+            print(f"[*] Extracting {dictionary_strings} dictionary")
             z = zipfile.ZipFile(os.path.basename(url))
         except zipfile.error as e:
             print(f"Bad zipfile (from {url!r}): {e}")
             return
         for n in z.namelist():
             print(n)
-            dest = os.path.join('./', n)
+            dest = os.path.join("./", n)
             destdir = os.path.dirname(dest)
             if not os.path.isdir(destdir):
                 os.makedirs(destdir)
             data = z.read(n)
-            f = open(dest, 'wb')
+            f = open(dest, "wb")
             f.write(data)
             f.close()
         z.close()
         os.unlink(name)
-    if dictionary_strings == 'opencorpora':
-        print(f'[*] Extracting {dictionary_strings} dictionary')
+    if dictionary_strings == "opencorpora":
+        print(f"[*] Extracting {dictionary_strings} dictionary")
         uncompresseddata = bz2.BZ2File(os.path.basename(url)).read()
         zname = os.path.splitext(os.path.basename(url))[0]
-        f = open(zname, 'wb')
+        f = open(zname, "wb")
         f.write(uncompresseddata)
         f.close()
     return
 
 
 def autoclean(dictionary_strings):
-    print(f'[*] Autocleaning {dictionary_strings} dictionary')
-    if dictionary_strings == 'opencorpora':
-        name = os.path.splitext(
-            os.path.basename(dictionary_urls[dictionary_strings]))[0]
-    elif dictionary_strings == 'ruscorpora':
-        name = f'{os.path.splitext(os.path.basename(dictionary_urls[dictionary_strings]))[0]}.txt'
+    print(f"[*] Autocleaning {dictionary_strings} dictionary")
+    if dictionary_strings == "opencorpora":
+        name = os.path.splitext(os.path.basename(dictionary_urls[dictionary_strings]))[
+            0
+        ]
+    elif dictionary_strings == "ruscorpora":
+        name = f"{os.path.splitext(os.path.basename(dictionary_urls[dictionary_strings]))[0]}.txt"
     # stripping all digits and english characters
-    regex = re.compile(r'[a-zA-Z0-9_]')
-    with codecs.open(name, 'r', 'utf-8') as f1:
+    regex = re.compile(r"[a-zA-Z0-9_]")
+    with codecs.open(name, "r", "utf-8") as f1:
         lines = f1.read().splitlines()
-    if dictionary_strings == 'opencorpora':
+    if dictionary_strings == "opencorpora":
         lines = [x for x in lines if not regex.search(x.split()[0])]
         lines = [x for x in lines if len(x.split()[0]) > 3]
-    elif dictionary_strings == 'ruscorpora':
+    elif dictionary_strings == "ruscorpora":
         lines = [x for x in lines if not regex.search(x.split()[1])]
         lines = [x for x in lines if len(x.split()[1]) > 3]
 
-    with codecs.open('{dictionary_strings}dict_stripped}', 'w', 'utf-8') as f2:
-        if dictionary_strings == 'opencorpora':
+    with codecs.open("{dictionary_strings}dict_stripped}", "w", "utf-8") as f2:
+        if dictionary_strings == "opencorpora":
             for line in lines:
-                f2.write(f'{str(line).split()[0].lower()}\n')
-        elif dictionary_strings == 'ruscorpora':
+                f2.write(f"{str(line).split()[0].lower()}\n")
+        elif dictionary_strings == "ruscorpora":
             for line in lines:
-                f2.write(f'{str(line).split()[1].lower()}\n')
+                f2.write(f"{str(line).split()[1].lower()}\n")
 
     f1.close()
     f2.close()
     return
 
 
-def generatedictionary(source, destination,  gendic):
-    with codecs.open(source, 'r', 'utf-8') as f:
+def generatedictionary(source, destination, gendic):
+    with codecs.open(source, "r", "utf-8") as f:
         lines = f.read().splitlines()
-    print(f'[*] Making {gendic} dictionary: ')
+    print(f"[*] Making {gendic} dictionary: ")
     # TODO
-    if gendic == 'tran5l1t':
+    if gendic == "tran5l1t":
         print("Not implemented yet")
         return
-    if gendic == 'translit':
-        with codecs.open(destination, 'a+', 'utf-8') as myfile:
+    if gendic == "translit":
+        with codecs.open(destination, "a+", "utf-8") as myfile:
             for line in tqdm(lines):
                 myfile.write(f"{translit(str(line), 'ru', reversed=True)}\n")
 
-    if gendic == 'ru_inv_en':
-        with codecs.open(destination, 'a+', 'utf-8') as myfile:
+    if gendic == "ru_inv_en":
+        with codecs.open(destination, "a+", "utf-8") as myfile:
             for line in tqdm(lines):
                 myfile.write(f"{translit(str(line), gendic)}\n")
 
@@ -156,9 +164,9 @@ def compare_two_password_bases(source, destination, dictionary):
     leaked_passwords_name = source
     translit_dictionary_name = dictionary
     result_statistics_name = destination
-    with codecs.open(leaked_passwords_name, 'r', 'utf-8') as f:
+    with codecs.open(leaked_passwords_name, "r", "utf-8") as f:
         leaked_passwords = f.read().splitlines()
-    with codecs.open(translit_dictionary_name, 'r', 'utf-8') as content_file:
+    with codecs.open(translit_dictionary_name, "r", "utf-8") as content_file:
         translit_dictionary = content_file.read().splitlines()
     print("[*] Generating statistics: ")
     s = set(translit_dictionary)
@@ -167,10 +175,11 @@ def compare_two_password_bases(source, destination, dictionary):
     count = Counter(b3)
 
     print("[*] Writing to file: ")
-    with codecs.open(result_statistics_name, 'w+', 'utf-8') as myfile:
+    with codecs.open(result_statistics_name, "w+", "utf-8") as myfile:
         for k, v in count.most_common():
             myfile.write(
-                '%d ' % v + k + ' ' + translit(k, 'ru_inv_en', reversed=True) + '\n')
+                "%d " % v + k + " " + translit(k, "ru_inv_en", reversed=True) + "\n"
+            )
     myfile.close()
     print("Done")
     return
@@ -178,19 +187,35 @@ def compare_two_password_bases(source, destination, dictionary):
 
 def main():
 
-    parser = argparse.ArgumentParser(description=__banner__,
-                                     epilog='Usage: ./python crwg.py -g ru_inv_en -s sourcedict -d destinationdict \n   /or/   python crwg.py --downloaddictionaries ruscorpora --autoclean \n   /or/   ')
+    parser = argparse.ArgumentParser(
+        description=__banner__,
+        epilog="Usage: ./python crwg.py -g ru_inv_en -s sourcedict -d destinationdict \n   /or/   python crwg.py --downloaddictionaries ruscorpora --autoclean \n   /or/   ",
+    )
     parser.add_argument(
-        '--gendic', '-g', choices=['ru_inv_en', 'translit', 'tran5l1t'], help='Generate dictionary from file ')
-    parser.add_argument('--downloaddictionaries', choices=[
-                        'ruscorpora', 'opencorpora'], help='Download ruscorpora or opencorpora')
-    parser.add_argument('--autoclean', action='store_true',
-                        help='Autoclean downloaded dictionaries (english characters, numbers, etc)')
-    parser.add_argument('--source', '-s', help="Source file")
-    parser.add_argument('--destination', '-d', help='Destination file')
-    parser.add_argument('--dictionary', help='Dictionary file')
-    parser.add_argument('--compare_two_password_bases', '-c',
-                        action='store_true', help='Compare two password files')
+        "--gendic",
+        "-g",
+        choices=["ru_inv_en", "translit", "tran5l1t"],
+        help="Generate dictionary from file ",
+    )
+    parser.add_argument(
+        "--downloaddictionaries",
+        choices=["ruscorpora", "opencorpora"],
+        help="Download ruscorpora or opencorpora",
+    )
+    parser.add_argument(
+        "--autoclean",
+        action="store_true",
+        help="Autoclean downloaded dictionaries (english characters, numbers, etc)",
+    )
+    parser.add_argument("--source", "-s", help="Source file")
+    parser.add_argument("--destination", "-d", help="Destination file")
+    parser.add_argument("--dictionary", help="Dictionary file")
+    parser.add_argument(
+        "--compare_two_password_bases",
+        "-c",
+        action="store_true",
+        help="Compare two password files",
+    )
 
     if len(sys.argv) == 1:
         parser.print_help()
@@ -203,17 +228,24 @@ def main():
     if args.autoclean:
         autoclean(args.downloaddictionaries)
 
-    if args.gendic and not(args.source and args.destination):
+    if args.gendic and not (args.source and args.destination):
         print("You must specify source and destination")
     elif args.gendic and args.source and args.destination:
         generatedictionary(args.source, args.destination, args.gendic)
 
-    if args.compare_two_password_bases and not(args.source and args.destination and args.dictionary):
+    if args.compare_two_password_bases and not (
+        args.source and args.destination and args.dictionary
+    ):
         print(
-            "Usage: python crwg.py -c -s leaked_passwords -d statistics --dictionary ./opencorporadict_stripped_ru_inv_en")
-    if args.compare_two_password_bases and args.source and args.destination and args.dictionary:
-        compare_two_password_bases(
-            args.source, args.destination, args.dictionary)
+            "Usage: python crwg.py -c -s leaked_passwords -d statistics --dictionary ./opencorporadict_stripped_ru_inv_en"
+        )
+    if (
+        args.compare_two_password_bases
+        and args.source
+        and args.destination
+        and args.dictionary
+    ):
+        compare_two_password_bases(args.source, args.destination, args.dictionary)
 
 
 main()
