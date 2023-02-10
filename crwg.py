99d4c417564e4ee635f9928248b18d64e874b80e
lmsecure <lmsecure@lianmedia.ru>
2023-02-10 09:09:42 +0500

    Added a support of special characters

--- crwg.py
+++ crwg.py
@@ -43,8 +43,8 @@ class ReverseInverseRussianLanguagePack(TranslitLanguagePack):
     language_code = "ru_inv_en"
     language_name = "ru_inv_en"
     mapping = (
-        "йцукенгшщзхъфывапролджэёячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЁЯЧСМИТЬБЮ",
-        "qwertyuiop[]asdfghjkl;'`zxcvbnm,.QWERTYUIOP{}ASDFGHJKL:\"~ZXCVBNM<>?",
+        '''"№;%:?йцукенгшщзхъфывапролджэёячсмитьбюйЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЁЯЧСМИТЬБЮ''',
+        '''@#$%^&qwertyuiop[]asdfghjkl;'`zxcvbnm,.QWERTYUIOP{}ASDFGHJKL:"~ZXCVBNM<>?''',
     )
 
 
@@ -52,8 +52,8 @@ class ReverseInverseEnglishLanguagePack(TranslitLanguagePack):
     language_code = "en_inv_ru"
     language_name = "en_inv_ru"
     mapping = (
-        "qwertyuiop[]asdfghjkl;'`zxcvbnm,.QWERTYUIOP{}ASDFGHJKL:\"~ZXCVBNM<>?",
-        "йцукенгшщзхъфывапролджэёячсмитьбюйЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЁЯЧСМИТЬБЮ",
+        '''@#$%^&qwertyuiop[]asdfghjkl;'`zxcvbnm,.QWERTYUIOP{}ASDFGHJKL:"~ZXCVBNM<>?''',
+        '''"№;%:?йцукенгшщзхъфывапролджэёячсмитьбюйЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЁЯЧСМИТЬБЮ''',
     )
 
 
